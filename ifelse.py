d769246f47ecb376a5d7e8851ad77723fd375ac6
Jeberson Yabez.c <jebersonyabez@gmail.com>
2026-07-12 23:08:55 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+username="Jeberson"
+password="yabez"
+
+inputusername=input("Enter the user name:")
+inputpassword=input("Enter the password:")
+
+if(username==inputusername):
+    print("Logged in")
+else:
+    print("Login failed")
---
+++ operators.py
@@ -0,0 +1,26 @@
+#a=10
+#b=15
+"""
+c=a+b
+print(c)
+"""
+
+#operators
+"""""
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+"""""
+
+a=int(input("Enter the value of a:"))
+""""""
+a=input("Enter the value of a:")
+a=int(a)
+""""""
+b=int(input("Enter the value of b:"))
+
+c=a+b
+
+print(c)
