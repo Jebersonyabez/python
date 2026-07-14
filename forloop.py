9c0d196e415d917182441f7fe4d5af3ed3036c49
Jeberson Yabez.c <jebersonyabez@gmail.com>
2026-07-14 23:41:43 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,33 @@
+#for i in range (5):
+  #  print(i)
+
+#for i in range(1,5):
+ #   print(i)
+
+#for i in range(0,10,2):
+  #  print(i)
+
+#for i in range(10,0,-1):
+ #   print(i)
+
+#Method 1 to print the list using range function
+
+number=[10,20,30,40,50]
+
+#for num in range(len(number)):
+ #   print(num, number[num])
+
+
+#Method 2 without using range
+
+
+
+#for num in  number:
+   # print(num)
+
+#for num in number:
+   # if(num==40):
+      #  number.append(60)
+    # print(num)
+
+
